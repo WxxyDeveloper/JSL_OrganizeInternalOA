52d912541734d8a6a3bbbcd7eb48811f6108fd90
XiaoLFeng <gm@x-lf.cn>
2023-12-20 19:55:07 +0800

    Initial comment

---
+++ src/main/java/com/jsl/oa/JslOrganizeInternalOaApplication.java
@@ -0,0 +1,13 @@
+package com.jsl.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JslOrganizeInternalOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JslOrganizeInternalOaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsl/oa/JslOrganizeInternalOaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsl.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JslOrganizeInternalOaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
