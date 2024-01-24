232899e4c314f56aab947cfe9545c9a24b8ee6c3
XiaoLFeng <gm@x-lf.cn>
2024-01-24 17:20:59 +0800

    自动化部署打包测试

--- src/main/java/com/jsl/oa/controllers/IndexController.java
+++ src/main/java/com/jsl/oa/controllers/IndexController.java
@@ -3,8 +3,11 @@ package com.jsl.oa.controllers;
 import com.jsl.oa.utils.BaseResponse;
 import com.jsl.oa.utils.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @author xiaofeng
@@ -12,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class IndexController {
+    @Value("${maven.timestamp}")
+    private String timestamp;
+
     @RequestMapping("/")
     public BaseResponse index() {
         log.info("请求接口[GET]: /");
-        return ResultUtil.success("欢迎使用JSL-OA系统，服务器处于正常状态");
+        HashMap<String, String> newMap = new HashMap<>();
+        newMap.put("timestamp", "构建时间：" + timestamp);
+        return ResultUtil.success("欢迎使用JSL-OA系统，服务器处于正常状态", newMap);
     }
 }
