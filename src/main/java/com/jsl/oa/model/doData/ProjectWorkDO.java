7596e1972147023b66c71c7ad8ef4fab9f6821de
xiangZr-hhh <2774915446@qq.com>
2024-03-12 14:26:51 +0800

    用户信息接口修正

--- src/main/java/com/jsl/oa/model/doData/ProjectWorkDO.java
+++ src/main/java/com/jsl/oa/model/doData/ProjectWorkDO.java
@@ -1,16 +1,28 @@
+
 package com.jsl.oa.model.doData;
-/*
-        张睿相   Java
-*/
 
-/**
- * 总体描述
- * <p>创建时间：2024/3/11 21:54</p>
- *
- * @author 张睿相
- * @since v1.0
- */
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.sql.Timestamp;
+
+@Data
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProjectWorkDO {
+    private Long id;
+    private Long pid;
+    private Long projectId;
+    private Long principalId;
+    private Integer cycle;
+    private Integer workLoad;
+    private Integer type;
+    private String name;
+    private String description;
+    private Integer isDelete;
+    private Integer isFinish;
+    private boolean status;
+    private Timestamp beginTime;
+    private Timestamp completeTime;
 }
-
-
