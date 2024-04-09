b48d89f22e11c26f9487424172506b652cc1d78c
XiaoLFeng <gm@x-lf.cn>
2024-04-09 20:46:54 +0800

    debug: 内容 Debug 测试提交

--- src/main/java/com/jsl/oa/JslOrganizeInternalOaApplication.java
+++ src/main/java/com/jsl/oa/JslOrganizeInternalOaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class JslOrganizeInternalOaApplication {
-// debug
     public static void main(String[] args) {
         SpringApplication.run(JslOrganizeInternalOaApplication.class, args);
     }
