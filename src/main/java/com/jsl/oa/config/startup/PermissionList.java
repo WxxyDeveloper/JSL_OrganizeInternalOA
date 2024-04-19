75d9435911d35d9f7dd5058a74ff602ccab5f732
XiaoLFeng <gm@x-lf.cn>
2024-04-19 19:12:26 +0800

    fix(业务): 权限修改
    对于误修改部分进行补丁

--- src/main/java/com/jsl/oa/config/startup/PermissionList.java
+++ src/main/java/com/jsl/oa/config/startup/PermissionList.java
@@ -23,22 +23,22 @@ public class PermissionList {
         permissionList.add(new PermissionVO("info:get_header_image", "获取头部图片"));
         permissionList.add(new PermissionVO("info:edit_header_image", "编辑头部图片"));
         permissionList.add(new PermissionVO("info:delete_header_image", "删除头部图片"));
-        permissionList.add(new PermissionVO("project:child:add", "增加子系统"));
-        permissionList.add(new PermissionVO("project:module:add", "增加子模块"));
+        permissionList.add(new PermissionVO("project:child_add", "增加子系统"));
+        permissionList.add(new PermissionVO("project:module_add", "增加子模块"));
 
         permissionPrincipal.add(new PermissionVO("auth:change_password", "修改密码"));
         permissionPrincipal.add(new PermissionVO("info:get_header_image", "获取头部图片"));
         permissionPrincipal.add(new PermissionVO("info:edit_header_image", "编辑头部图片"));
         permissionPrincipal.add(new PermissionVO("info:delete_header_image", "删除头部图片"));
         permissionPrincipal.add(new PermissionVO("project:add", "增加项目"));
-        permissionPrincipal.add(new PermissionVO("project:child:add", "增加子系统"));
-        permissionPrincipal.add(new PermissionVO("project:module:add", "增加子模块"));
+        permissionPrincipal.add(new PermissionVO("project:child_add", "增加子系统"));
+        permissionPrincipal.add(new PermissionVO("project:module_add", "增加子模块"));
 
         permissionDeveloper.add(new PermissionVO("auth:change_password", "修改密码"));
         permissionDeveloper.add(new PermissionVO("info:get_header_image", "获取头部图片"));
         permissionDeveloper.add(new PermissionVO("info:edit_header_image", "编辑头部图片"));
         permissionDeveloper.add(new PermissionVO("info:delete_header_image", "删除头部图片"));
-        permissionDeveloper.add(new PermissionVO("project:child:add", "增加子系统"));
-        permissionDeveloper.add(new PermissionVO("project:module:add", "增加子模块"));
+        permissionDeveloper.add(new PermissionVO("project:child_add", "增加子系统"));
+        permissionDeveloper.add(new PermissionVO("project:module_add", "增加子模块"));
     }
 }
